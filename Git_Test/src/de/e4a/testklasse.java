3f7a0edd87859231383e3e4512a476be5f495c3d
petrichmatthias <petrich.matthias@web.de>
2014-02-18 09:02:14 +0100

    Zahlen Ausgeben

--- Git_Test/src/de/e4a/testklasse.java
+++ Git_Test/src/de/e4a/testklasse.java
@@ -1,5 +1,27 @@
 package de.e4a;
 
 public class testklasse {
+	public static void main(String[] args) {
 
+		int[] zahlen = { 1, 2, 3, 4, 5 };
+
+		// Ausgabe mit foreach
+		for (int z : zahlen) {
+			System.out.println(z);
+		}
+
+		// Ausgabe mit for
+		for (int index = 0; index < zahlen.length; index++) {
+			System.out.println(zahlen[index]);
+		}
+
+		// ausgabe mit while
+
+		int index = 0;
+		while (index < zahlen.length) {
+			System.out.println(zahlen[index]);
+			index++;
+		}
+
+	}
 }
